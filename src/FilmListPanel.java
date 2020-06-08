aa25c2024b2069af704ca59c187bb9b1686c8418
tymoteusz <tymekchelmecki@gmail.com>
2020-06-08 19:45:35 +0200

    radioButton

--- src/FilmListPanel.java
+++ src/FilmListPanel.java
@@ -4,9 +4,7 @@ import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class FilmListPanel extends JPanel {
-    private ArrayList<JRadioButton> check;
     private ArrayList<JLabel>  label;
-    private ButtonGroup bg= new ButtonGroup();
     private Font font = new Font("SanSerif",Font.BOLD,20);
     private String title;
     JScrollPane scrollPane;
@@ -18,11 +16,8 @@ public class FilmListPanel extends JPanel {
         panel.setLayout(null);
         title = t.toLowerCase();
 
-        check = new ArrayList();
-
-
         JLabel Title = new JLabel(title);
-        Title.setBounds(110, 10, 500, 25);
+        Title.setBounds(50, 10, 500, 40);
         Title.setFont(font);
         add(Title);
         panel.setLayout(null);
@@ -30,17 +25,16 @@ public class FilmListPanel extends JPanel {
         label = new ArrayList();
         ArrayList<String> s=Window.getApp().Print(Window.getApp().Select("SELECT * FROM "+t+";"));
         for (int i = 0; i < s.size(); i++) {
-            check.add(new JRadioButton(s.get(i)));
-            bg.add(check.get(i));
-            check.get(i).setBounds(10, 10 + 35 * i + 40, 3000, 25);
-            panel.add(check.get(i));
+            label.add(new JLabel(s.get(i)));
+            label.get(i).setBounds(10, 10 + 35 * i, 3000, 25);
+            panel.add(label.get(i));
         }
 
 
         panel.setPreferredSize(new Dimension(400,300));
         JScrollPane scrollPane = new JScrollPane(panel);
 
-        scrollPane.setBounds(50, 30, 500, 300);
+        scrollPane.setBounds(50, 50, 500, 300);
 
         add(scrollPane);
     }
